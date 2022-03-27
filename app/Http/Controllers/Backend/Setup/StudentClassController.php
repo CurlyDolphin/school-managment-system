e78e5010f9d531119ec952e013f4b441e86ae3f0
CurlyDolphin <dsss.333@yandex.ru>
2022-03-27 14:17:03 +0300

    Make base student control

---
+++ app/Http/Controllers/Backend/Setup/StudentClassController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Setup;
+
+use App\Http\Controllers\Controller;
+use App\Models\StudentClass;
+use Illuminate\Http\Request;
+
+class StudentClassController extends Controller
+{
+    public function ViewStudent() {
+        $data['allData'] = StudentClass::all();
+        return view('backend.setup.student_class.view_class', $data);
+    }
+}
